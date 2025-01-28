0309e1f2a4c7bc413652a71c18e80aa88a1653ae
Pizza-code-8 <ggpllagg@gmail.com>
2025-01-28 13:35:07 +0700

    Update callbacks.py

--- handlers_callbacks/callbacks.py
+++ handlers_callbacks/callbacks.py
@@ -156,7 +156,7 @@ async def choose_category(callback: CallbackQuery, callback_data: MgFactory_clie
     mg_stuff[callback.from_user.id] = await state.get_data()
     try:
         id = take_stuff_id_from_stuff_by_name_mg(name, mg)
-        for_send = FSInputFile(f"/home/sns/SNS_SHOP/{id}.jpg")
+        for_send = FSInputFile(f"/home/sns/SNS_SHOP/{id}.jpg", filename=f"{id}.jpg")
         await bot.send_photo(
             chat_id=callback.from_user.id,
             photo=for_send,
@@ -749,7 +749,7 @@ async def choose_category(callback: CallbackQuery, callback_data: MgFactory, sta
     cat_name[callback.from_user.id] = await state.get_data()
     try:
         id = take_stuff_id_from_stuff_by_name_mg(name, mg)
-        for_send = FSInputFile(f"/home/sns/SNS_SHOP/{id}.jpg")
+        for_send = FSInputFile(f"/home/sns/SNS_SHOP/{id}.jpg", filename=f"{id}.jpg")
         await bot.send_photo(
             chat_id=callback.from_user.id,
             photo=for_send,
