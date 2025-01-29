1c6d5fd4d6c5d7ed9a9ce7199a8e94178bdb3008
Pizza-code-8 <ggpllagg@gmail.com>
2025-01-29 14:34:47 +0700

    Update callbacks.py

--- handlers_callbacks/callbacks.py
+++ handlers_callbacks/callbacks.py
@@ -19,7 +19,7 @@ from kb.kb import main_menu_kb
 from db.sns_users import take_ref_link, take_referals, take_stat_ref, take_discount, add_basket, take_basket, take_user_id, take_username, clean_basket, take_id_orders, add_orders_in_users, add_user, check_user, take_order_check, add_order_check, add_referals, add_username_by_id
 from db.category import add_num_category, add_name_in_category, add_price_in_category, take_all_category, delete_category, add_weight_category, add_portions_category, take_name_category_from_category, take_price_from_category_by_name, take_weight_from_category_by_name, take_portions_from_category_by_name
 from db.stuff import add_name_in_stuff, add_category_name_in_stuff, add_num_in_stuff, take_stuff_id_from_stuff, delete_stuff, take_cat_name_from_stuff, add_in_stock_in_stuff, take_in_stock_from_stuff, take_all_stuff_id_from_stuff, take_cat_name_from_stuff_by_id, take_stuff_name_from_stuff, add_in_stock_in_stuff_by_id, add_mg_in_stuff_by_num, take_mg_by_name_by_mg, take_cat_name_from_stuff_by_mg, take_stuff_name_from_stuff_by_mg, take_stuff_id_from_stuff_by_name_mg
-from db.orders import add_user_id_orders, add_adress_in_orders, add_basket_in_orders, add_sum_in_orders, take_adress_from_orders, take_sum_from_orders, take_username_from_orders, take_basket_from_orders, clean_order, add_phone_in_orders, take_phone_from_orders, take_stat_from_orders, add_stat_in_orders, take_time_of_order, take_username_from_orders_by_uid
+from db.orders import add_user_id_orders, add_adress_in_orders, add_basket_in_orders, add_sum_in_orders, take_adress_from_orders, take_sum_from_orders, take_username_from_orders, take_basket_from_orders, clean_order, add_phone_in_orders, take_phone_from_orders, take_stat_from_orders, add_stat_in_orders, take_time_of_order, take_username_from_orders_by_uid, add_username_in_orders
 from aiogram.utils.deep_linking import create_start_link
 
 router = Router()
@@ -320,7 +320,8 @@ async def address_order(message: Message, state: FSMContext):
     price = order_price[message.from_user.id]["price"]
     n = address[message.from_user.id]["address"]
     id = "".join(random.choices(string.ascii_letters + string.digits, k=15))
-    add_user_id_orders(user_id=uid, username=username, order_id=id, time = now)
+    add_user_id_orders(user_id=uid, order_id=id, time = now)
+    add_username_in_orders(user_id=uid, u_name=username)
     add_adress_in_orders(id, n)
     add_phone_in_orders(id, phone_num)
     add_sum_in_orders(id, price)
